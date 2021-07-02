a9831d46f67e85d5a28e68f16428490ef48615a7
syavuz18 <66845247+syavuz18@users.noreply.github.com>
2021-07-02 15:38:44 +0300

    Add files via upload

---
+++ HelloSpringBoot/src/main/java/com/example/hellospringboot/Controller.java
@@ -0,0 +1,21 @@
+package com.example.hellospringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class Controller {
+
+    private static final String template = "Hello %s !";
+    private static AtomicInteger counter = new AtomicInteger();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+}
---
+++ HelloSpringBoot/src/main/java/com/example/hellospringboot/Greeting.java
@@ -0,0 +1,20 @@
+package com.example.hellospringboot;
+
+public class Greeting {
+
+    private final int id;
+    private final String content;
+
+    public Greeting(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
